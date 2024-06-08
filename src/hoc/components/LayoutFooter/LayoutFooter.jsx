bd599ab590c47925bf76c7590aaa5d0ef277d502
Lưu Quang Vũ <vulqse151185@fpt.edu.vn>
2024-06-08 12:39:49 +0700

    style: update footer

--- src/hoc/components/LayoutFooter/LayoutFooter.jsx
+++ src/hoc/components/LayoutFooter/LayoutFooter.jsx
@@ -1,45 +1,51 @@
-import Icon from '@ant-design/icons/lib/components/Icon'
-import React from 'react'
+import React from 'react';
+import { Layout, Row, Col, Typography } from 'antd';
+import { FacebookOutlined, YoutubeOutlined, GoogleOutlined } from '@ant-design/icons';
+import './styles.scss';
+
+const { Footer } = Layout;
+const { Title, Text } = Typography;
 
 const LayoutFooter = () => {
-  <div className='bg-purple text-white p-20 flex items-center'>
-          <div className="w-1/3 px-10 text-center">
-                <div className="front-medium text-2x1">Tên Công ty</div>
-                <div className='text-2xl font-light my-5'>Tên trang Web</div>
-                <div>
-                Học không biết chán, dạy người không biết mỏi.
-                </div>
-                <div className="font-medium text-xl italic my-5">Liên Hệ</div>
-                <div className="flex justify-between px-28 text-4xl">
-                  <Icon icon="ic:baseline-facebook"/>
-                  <Icon icon="ic:baseline-youtube"/>
-                  <Icon icon="ri:google-fill" style={{color:"white"}}/>
-                </div>
+  return (
+    <Footer className="footer">
+      <Row className="footer-item" gutter={[16, 16]}>
+        <Col span={8} className="footer-left">
+          <Title level={2} className="cpn-name">Tên Công ty</Title>
+          <Text className="web-name">Tên trang Web</Text>
+          <div className="text">
+            Học không biết chán, dạy người không biết mỏi.
           </div>
-          <div className="w-1/3 px-10 italic">
-          <div className="text-2xl font-medium">Hà Nội</div>
-        <div>
-          Tầng 7 15 & 20, Tòa nhà Capital - 109 Trần Hưng Đạo, Quận Hoàn Kiếm,
-          Hà Nội, Việt Name
-        </div>
-        <div className="text-2xl font-medium mt-5">TP.Hồ Chí Minh</div>
-        <div>
-          Tầng 29 & 30, Tòa nhà LIM 9-11 Tôn Đức Thắng, Quận 1, Thành phố Hồ Chí
-          Minh, Việt Nam
-        </div>
+          <Title level={4} className="web-contact">Liên Hệ</Title>
+          <div className="icon" style={{ color: "white" }}>
+            <FacebookOutlined />
+            <YoutubeOutlined />
+            <GoogleOutlined  />
           </div>
-          <div className="w-1/3 px-10 italic">
-        <div className="text-2xl font-medium">Văn phòng Gia sư Quận Gò Vấp</div>
-        <div>
-        672A28, Đường Phan Văn Trị, Phường 10, Quận Gò Vấp 
-        Khu CityLand Park Hill
-        </div>
-        <div className="text-2xl font-medium mt-5">Văn phòng Gia sư Đất Việt Quận Gò Vấp</div>
-        <div>
-        672A28, Đường Phan Văn Trị, Phường 10, Quận Gò Vấp 
-        Khu CityLand Park Hill
-        </div>
-  </div>
-  </div>
-}
-export default LayoutFooter
+        </Col>
+        <Col span={8} className="footer-m">
+          <Title level={4} className="location-name">Hà Nội</Title>
+          <div className='text'>
+            Tầng 7 15 & 20, Tòa nhà Capital - 109 Trần Hưng Đạo, Quận Hoàn Kiếm, Hà Nội, Việt Nam
+          </div>
+          <Title level={4} className="location-name">TP.Hồ Chí Minh</Title>
+          <div className='text'>
+            Tầng 29 & 30, Tòa nhà LIM 9-11 Tôn Đức Thắng, Quận 1, Thành phố Hồ Chí Minh, Việt Nam
+          </div>
+        </Col>
+        <Col span={8} className="footer-r">
+          <Title level={4} className="location-name">Văn phòng Gia sư Quận Gò Vấp</Title>
+          <div className='text'>
+            672A28, Đường Phan Văn Trị, Phường 10, Quận Gò Vấp Khu CityLand Park Hill
+          </div>
+          <Title level={4} className="location-name">Văn phòng Gia sư Đất Việt Quận Gò Vấp</Title>
+          <div className='text'>
+            672A28, Đường Phan Văn Trị, Phường 10, Quận Gò Vấp Khu CityLand Park Hill
+          </div>
+        </Col>
+      </Row>
+    </Footer>
+  );
+};
+
+export default LayoutFooter;
